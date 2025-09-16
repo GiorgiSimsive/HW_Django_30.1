3e544cf6efb8d6a958fed0c41282926def149cc5
adminnn <geojesi1@mail.ru>
2025-09-16 15:39:14 +0400

    fix

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 INSTALLED_APPS = [
